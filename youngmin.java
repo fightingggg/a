8a53296140dbf657fb9af94e656386e3cef524aa
fightingggg <121657134+fightingggg@users.noreply.github.com>
2022-12-31 21:48:10 +0900

    Add files via upload

---
+++ youngmin.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class youngmin {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("몇단인지 입력");
+
+		int a = s.nextInt();
+		int b = 1;
+		
+		System.out.println(a+"x"+b+"="+(a*b));
+		b=b+1;
+		System.out.println(a+"x"+b+"="+(a*b));
+		b=b+1;System.out.println(a+"x"+b+"="+(a*b));
+		b=b+1;System.out.println(a+"x"+b+"="+(a*b));
+		b=b+1;System.out.println(a+"x"+b+"="+(a*b));
+		b=b+1;System.out.println(a+"x"+b+"="+(a*b));
+		b=b+1;System.out.println(a+"x"+b+"="+(a*b));
+		b=b+1;System.out.println(a+"x"+b+"="+(a*b));
+		b=b+1;System.out.println(a+"x"+b+"="+(a*b));
+		b=b+1;
+		b=b+1;System.out.println(a+"x"+b+"="+(a*b));
+		b=b+1;System.out.println(a+"x"+b+"="+(a*b));
+		b=b+1;System.out.println(a+"x"+b+"="+(a*b));
+		b=b+1;System.out.println(a+"x"+b+"="+(a*b));
+		b=b+1;System.out.println(a+"x"+b+"="+(a*b));
+		b=b+1;System.out.println(a+"x"+b+"="+(a*b));
+		b=b+1;System.out.println(a+"x"+b+"="+(a*b));
+	}
+
+}
